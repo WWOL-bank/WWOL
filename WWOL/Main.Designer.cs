5b60a210eb707ad2c6535f1a8bee236c928c07fc
yjnoh1016 <yjnoh1016@naver.com>
2021-06-26 21:13:14 +0900

    [UPDATE] SignIn Page Publish

--- WWOL/Main.Designer.cs
+++ WWOL/Main.Designer.cs
@@ -29,20 +29,81 @@ namespace WWOL
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.Button dissaving;
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
-            this.button1 = new System.Windows.Forms.Button();
+            this.transfer = new System.Windows.Forms.Button();
+            this.check = new System.Windows.Forms.Button();
+            this.deposit = new System.Windows.Forms.Button();
+            this.time = new System.Windows.Forms.Label();
+            dissaving = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // button1
+            // dissaving
             // 
-            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(246)))), ((int)(((byte)(251)))), ((int)(((byte)(255)))));
-            this.button1.Font = new System.Drawing.Font("한컴 고딕", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
-            this.button1.Location = new System.Drawing.Point(155, 127);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(250, 85);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "예금 인출";
-            this.button1.UseVisualStyleBackColor = false;
+            dissaving.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(87)))), ((int)(((byte)(130)))), ((int)(((byte)(212)))));
+            dissaving.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("dissaving.BackgroundImage")));
+            dissaving.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(40)))), ((int)(((byte)(141)))));
+            dissaving.FlatAppearance.BorderSize = 0;
+            dissaving.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            dissaving.Font = new System.Drawing.Font("한컴 고딕", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            dissaving.Location = new System.Drawing.Point(236, 63);
+            dissaving.Name = "dissaving";
+            dissaving.Size = new System.Drawing.Size(250, 85);
+            dissaving.TabIndex = 0;
+            dissaving.UseVisualStyleBackColor = false;
+            // 
+            // transfer
+            // 
+            this.transfer.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(87)))), ((int)(((byte)(130)))), ((int)(((byte)(212)))));
+            this.transfer.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("transfer.BackgroundImage")));
+            this.transfer.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(40)))), ((int)(((byte)(141)))));
+            this.transfer.FlatAppearance.BorderSize = 0;
+            this.transfer.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.transfer.Font = new System.Drawing.Font("한컴 고딕", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            this.transfer.Location = new System.Drawing.Point(639, 396);
+            this.transfer.Name = "transfer";
+            this.transfer.Size = new System.Drawing.Size(250, 85);
+            this.transfer.TabIndex = 1;
+            this.transfer.UseVisualStyleBackColor = false;
+            // 
+            // check
+            // 
+            this.check.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(87)))), ((int)(((byte)(130)))), ((int)(((byte)(212)))));
+            this.check.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("check.BackgroundImage")));
+            this.check.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(40)))), ((int)(((byte)(141)))));
+            this.check.FlatAppearance.BorderSize = 0;
+            this.check.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.check.Font = new System.Drawing.Font("한컴 고딕", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            this.check.Location = new System.Drawing.Point(639, 167);
+            this.check.Name = "check";
+            this.check.Size = new System.Drawing.Size(250, 85);
+            this.check.TabIndex = 2;
+            this.check.UseVisualStyleBackColor = false;
+            // 
+            // deposit
+            // 
+            this.deposit.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(87)))), ((int)(((byte)(130)))), ((int)(((byte)(212)))));
+            this.deposit.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("deposit.BackgroundImage")));
+            this.deposit.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(40)))), ((int)(((byte)(141)))));
+            this.deposit.FlatAppearance.BorderSize = 0;
+            this.deposit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.deposit.Font = new System.Drawing.Font("한컴 고딕", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            this.deposit.Location = new System.Drawing.Point(236, 252);
+            this.deposit.Name = "deposit";
+            this.deposit.Size = new System.Drawing.Size(250, 85);
+            this.deposit.TabIndex = 3;
+            this.deposit.UseVisualStyleBackColor = false;
+            // 
+            // time
+            // 
+            this.time.AutoSize = true;
+            this.time.BackColor = System.Drawing.Color.White;
+            this.time.Font = new System.Drawing.Font("한컴 고딕", 50F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Pixel);
+            this.time.Location = new System.Drawing.Point(322, 625);
+            this.time.Name = "time";
+            this.time.Size = new System.Drawing.Size(229, 65);
+            this.time.TabIndex = 4;
+            this.time.Text = "현재 시각";
             // 
             // Main
             // 
@@ -50,16 +111,24 @@ namespace WWOL
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.ClientSize = new System.Drawing.Size(1008, 729);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.time);
+            this.Controls.Add(this.deposit);
+            this.Controls.Add(this.check);
+            this.Controls.Add(this.transfer);
+            this.Controls.Add(dissaving);
             this.Name = "Main";
             this.Text = "Main";
             this.Load += new System.EventHandler(this.Main_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button transfer;
+        private System.Windows.Forms.Button check;
+        private System.Windows.Forms.Button deposit;
+        private System.Windows.Forms.Label time;
     }
 }
---
+++ WWOL/Signin.Designer.cs
@@ -0,0 +1,136 @@
+﻿
+namespace WWOL
+{
+    partial class Signin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Signin));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(87)))), ((int)(((byte)(130)))), ((int)(((byte)(212)))));
+            this.label1.Font = new System.Drawing.Font("한컴 고딕", 80F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Pixel);
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(364, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(345, 104);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "회원가입";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(87)))), ((int)(((byte)(130)))), ((int)(((byte)(212)))));
+            this.label2.Font = new System.Drawing.Font("한컴 고딕", 45F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Pixel);
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(293, 203);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(109, 58);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "이름";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(87)))), ((int)(((byte)(130)))), ((int)(((byte)(212)))));
+            this.label3.Font = new System.Drawing.Font("한컴 고딕", 45F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Pixel);
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(281, 324);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(193, 58);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "비밀번호";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("한컴 고딕", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            this.textBox1.Location = new System.Drawing.Point(476, 206);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(366, 59);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("한컴 고딕", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            this.textBox2.Location = new System.Drawing.Point(476, 327);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(366, 59);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.UseSystemPasswordChar = true;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(87)))), ((int)(((byte)(130)))), ((int)(((byte)(212)))));
+            this.button1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button1.BackgroundImage")));
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("한컴 고딕", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            this.button1.Location = new System.Drawing.Point(789, 450);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(160, 85);
+            this.button1.TabIndex = 5;
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // Signin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(1008, 729);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Signin";
+            this.Text = "Signin";
+            this.Load += new System.EventHandler(this.Signin_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+    }
+}
